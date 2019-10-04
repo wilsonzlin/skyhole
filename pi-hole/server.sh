4a1c844e930bff1d1d8433649b1dfea399692c66
Wilson Lin <contact@wilsonl.in>
2019-10-04 23:45:15 +1000

    Fix sed and make quieter

--- pi-hole/server.sh
+++ pi-hole/server.sh
@@ -51,7 +51,7 @@ if [ -z "$PASSWORD" ]; then error "No password"; fi
 
 # Install system updates.
 sudo apt update
-sudo apt dist-upgrade -y
+sudo apt dist-upgrade -yq
 sudo apt install -y software-properties-common
 
 # Install Nginx.
@@ -64,12 +64,12 @@ sudo systemctl stop nginx
 
 # Install Stubby.
 sudo apt install -y stubby
-sudo sed -i /etc/stubby/stubby.yml 's%listen_addresses:\n  - 127.0.0.1\n  -  0::1%listen_addresses:\n  - 127.0.0.2\n  -  0::2'
+sudo sed -i /etc/stubby/stubby.yml 's%listen_addresses:\n  - 127.0.0.1\n  -  0::1%listen_addresses:\n  - 127.0.0.2\n  -  0::2%'
 
 # Install Pi-hole.
-sed pi-hole-setup.conf "s%<<<password>>>%$PASSWORD" | sudo tee /etc/pihole/setupVars.conf
+sed pi-hole-setup.conf "s%<<<password>>>%$PASSWORD%" | sudo tee /etc/pihole/setupVars.conf
 curl -L https://install.pi-hole.net | bash /dev/stdin --unattended
-sed lighttpd.external.conf "s%<<<domain>>>%$DOMAIN" | sudo tee /etc/lighttpd/external.conf
+sed lighttpd.external.conf "s%<<<domain>>>%$DOMAIN%" | sudo tee /etc/lighttpd/external.conf
 
 # Install Certbot.
 # Do this last so that pre and post hooks work.
@@ -77,7 +77,7 @@ sudo add-apt-repository -y universe
 sudo add-apt-repository -y ppa:certbot/certbot
 sudo apt update
 sudo apt install -y certbot
-sed post-00-copy-cert.sh "s%<<<domain>>>%$DOMAIN" | sudo tee /etc/letsencrypt/renewal-hooks/post/00-copy-cert.sh
+sed post-00-copy-cert.sh "s%<<<domain>>>%$DOMAIN%" | sudo tee /etc/letsencrypt/renewal-hooks/post/00-copy-cert.sh
 sudo cp pre/* /etc/letsencrypt/renewal-hooks/pre/.
 sudo cp post/* /etc/letsencrypt/renewal-hooks/post/.
 certbot certonly --standalone --non-interactive --agree-tos -m "$EMAIL" -d "$DOMAIN"
