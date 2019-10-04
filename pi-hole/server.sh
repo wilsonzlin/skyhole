cc4e22f114069f0b6b0e7ec426b7e65c1f5488fe
Wilson Lin <contact@wilsonl.in>
2019-10-04 23:48:49 +1000

    Fix sed

--- pi-hole/server.sh
+++ pi-hole/server.sh
@@ -64,12 +64,13 @@ sudo systemctl stop nginx
 
 # Install Stubby.
 sudo apt install -y stubby
-sudo sed -i /etc/stubby/stubby.yml 's%listen_addresses:\n  - 127.0.0.1\n  -  0::1%listen_addresses:\n  - 127.0.0.2\n  -  0::2%'
+sudo sed -i /etc/stubby/stubby.yml 's/^  - 127.0.0.1$/ - 127.0.0.2/'
+sudo sed -i /etc/stubby/stubby.yml 's/^  - 0::1$/ - 0::2/'
 
 # Install Pi-hole.
-sed pi-hole-setup.conf "s%<<<password>>>%$PASSWORD%" | sudo tee /etc/pihole/setupVars.conf
+sed pi-hole-setup.conf "s/<<<password>>>/$PASSWORD/" | sudo tee /etc/pihole/setupVars.conf
 curl -L https://install.pi-hole.net | bash /dev/stdin --unattended
-sed lighttpd.external.conf "s%<<<domain>>>%$DOMAIN%" | sudo tee /etc/lighttpd/external.conf
+sed lighttpd.external.conf "s/<<<domain>>>/$DOMAIN/" | sudo tee /etc/lighttpd/external.conf
 
 # Install Certbot.
 # Do this last so that pre and post hooks work.
@@ -77,7 +78,7 @@ sudo add-apt-repository -y universe
 sudo add-apt-repository -y ppa:certbot/certbot
 sudo apt update
 sudo apt install -y certbot
-sed post-00-copy-cert.sh "s%<<<domain>>>%$DOMAIN%" | sudo tee /etc/letsencrypt/renewal-hooks/post/00-copy-cert.sh
+sed post-00-copy-cert.sh "s/<<<domain>>>/$DOMAIN/" | sudo tee /etc/letsencrypt/renewal-hooks/post/00-copy-cert.sh
 sudo cp pre/* /etc/letsencrypt/renewal-hooks/pre/.
 sudo cp post/* /etc/letsencrypt/renewal-hooks/post/.
 certbot certonly --standalone --non-interactive --agree-tos -m "$EMAIL" -d "$DOMAIN"
