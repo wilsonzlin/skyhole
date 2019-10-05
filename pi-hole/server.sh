c739461f7c5a635af3b79100ae1a07203c864ed9
Wilson Lin <contact@wilsonl.in>
2019-10-05 13:10:26 +1000

    Fix sed

--- pi-hole/server.sh
+++ pi-hole/server.sh
@@ -77,10 +77,8 @@ interface="$(echo $route | awk '{print $5}')"
 subnet=$(ip -oneline -family inet address show | grep "${ipv4}/" |  awk '{print $4}')
 # Run Pi-hole install script.
 # Subnet contains slashes, so use % for sed.
-sed \
-  "s%<<<ipv4>>>%$subnet%" \
-  "s%<<<interface>>>%$interface%" \
-  pi-hole-setup.conf | sudo tee /etc/pihole/setupVars.conf
+sed "s%<<<ipv4>>>%$subnet%; s%<<<interface>>>%$interface%" pi-hole-setup.conf | \
+  sudo tee /etc/pihole/setupVars.conf
 script="$(mktemp)"
 wget -O "$script" https://install.pi-hole.net
 sudo bash "$script" --unattended
